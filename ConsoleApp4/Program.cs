6efd6f3c5aedd3cee7088509bc8c8ea50705a929
Andrei Tambovtsev <ait27ru@gmail.com>
2024-11-09 18:00:30 +1300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿bool IsPalindrome(string input)
+{
+    int start = 0;
+    int end = input.Length - 1;
+
+    while (start < end)
+    {
+        while (start < end && !char.IsLetterOrDigit(input[start]))
+            start++;
+
+        while (start < end && !char.IsLetterOrDigit(input[end]))
+            end--;
+
+        if (char.ToLower(input[start]) != char.ToLower(input[end]))
+            return false;
+
+        start++;
+        end--;
+    }
+
+    return true;
+}
+
+void Check(string s, bool shouldBePalindrome)
+{
+    Console.WriteLine(IsPalindrome(s) == shouldBePalindrome ? "pass" : "FAIL");
+}
+
+
+Check("abcba", true);
+Check("abcde", false);
+Check("Mr owl ate my metal worm", true);
+Check("Never Odd Or Even", true);
+Check("Never Even Or Odd", false);
+
+/*
+ * OUTPUT:
+pass
+pass
+pass
+pass
+pass
+ * 
+ */
